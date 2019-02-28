ccc871556a0c8c09976439e18ccd681afb90388a
PhoenixProgrammer <35172317+PhoenixProgrammer@users.noreply.github.com>
2019-02-28 09:07:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ app.post('/move', (request, response) => {
                     if (!ObstacleOnPosition([TestThisField.x, TestThisField.y])) {
                         if (!FieldInArray(TestThisField, FreeFields)) {
                             i++;
-                            f = new field(x, y);
+                            f = new field(TestThisField.x, TestThisField.y);
                             FreeFields.push(f);
                             FreeDirections.push(direction);
                         }
